63b5fef89df6dd9dc0ca0223de273d4b1daff8c9
Shikizuma <myroslav.bashkirov@gmail.com>
2023-10-07 16:36:28 +0300

    Add connection to database

---
+++ DecentralizationGovUa/Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecentralizationGovUa.Data
+{
+    internal static class Context
+    {
+        public static IDbConnection Connection 
+        {
+            get 
+            {
+                return new SqlConnection("Server=localhost\\SQLEXPRESS;Database=DecentralizationGovUa;Trusted_Connection=True;");
+            }
+        }
+    }
+}
