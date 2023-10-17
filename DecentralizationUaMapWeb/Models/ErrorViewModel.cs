54c5e5e3c6a89086e0a3cd48eb370c8efdfa6c11
Shikizuma <myroslav.bashkirov@gmail.com>
2023-10-17 17:06:42 +0300

    add asp mvc

---
+++ DecentralizationUaMapWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DecentralizationUaMapWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
