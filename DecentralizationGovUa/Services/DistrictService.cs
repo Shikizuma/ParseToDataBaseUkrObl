c298c7c8b98f5d95e500d0251efbbfb248efdce6
Shikizuma <myroslav.bashkirov@gmail.com>
2023-10-19 17:03:48 +0300

    Add repositories and district service

---
+++ DecentralizationGovUa/Services/DistrictService.cs
@@ -0,0 +1,25 @@
+﻿using DecentralizationGovUa.Implements;
+using DecentralizationGovUa.Models.CommunModels;
+using DecentralizationGovUa.Models.DistrictModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecentralizationGovUa.Services
+{
+    public class DistrictService
+    {
+        public async Task<List<DistrictInfoModel>> GetDistricts()
+        {
+            string query = "{regions{title,area_id,id,population,square}}";
+
+            ParseDecentralizationGovUa<DistrictDataResponseModel> parseDecentralizationGovUa = new(query);
+
+            var regionData = await parseDecentralizationGovUa.Parse();
+
+            return regionData.Data.Data.Districts;
+        }
+    }
+}
